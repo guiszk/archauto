3f5e26bbe34fd9533663328c624e9e2dcfd5ae01
Guilherme Suzuki <guiszk@protonmail.com>
2020-06-02 17:42:45 -0300

    Added chrootauto.sh file

--- archauto.sh
+++ archauto.sh
@@ -84,39 +84,46 @@ export UUID
 # CONFIGURE FSTAB
 genfstab -U /mnt > /mnt/etc/fstab
 
+# ADD SCRIPT TO CHROOT
+cp .chrootauto.sh /mnt/usr/local/bin/chrootauto
+
+# RUN SCRIPT
+arch-chroot /mnt chrootauto
+
 # CHROOT
-#arch-chroot /mnt
+#
 
 # SETUP BOOTLOADER
-arch-chroot /mnt cd
-arch-chroot /mnt bootctl install
-arch-chroot /mnt mv /boot/loader/loader.conf /boot/loader/loader.conf.bac
-arch-chroot /mnt echo -e "timeout 5\ndefault arch" > /boot/loader/loader.conf
-arch-chroot /mnt echo -e "title ArchLinux\nlinux /vmlinuz-linux\ninitrd /initramfs-linux.img\n"options rw cryptdevice=UUID=$UUID":crypt" root=/dev/mapper/crypt > /boot/loader/entries/arch.conf
-
-# MKINITCPIO
-arch-chroot /mnt #sed -i 's/oldstring/newstring/g' /etc/mkinitcpio.conf
-arch-chroot /mnt cp /etc/mkinitcpio.conf /etc/mkinitcpio.conf.bac
-arch-chroot /mnt sed -i "s/HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)/HOOKS=(base udev block keyboard autodetect modconf resume shutdown filesystems encrypt fsck keymap)/g" /etc/mkinitcpio.conf
-arch-chroot /mnt mkinitcpio -p linux
-
-# USER
-arch-chroot /mnt echo -n "Enter username: "
-arch-chroot /mnt read UNAME
-arch-chroot /mnt useradd -m -G audio,video,wheel $UNAME
-arch-chroot /mnt echo "Changing user password."
-arch-chroot /mnt passwd $UNAME
-arch-chroot /mnt echo "Changing root password."
-arch-chroot /mnt passwd
-
-# BACKUP SUDOERS
-arch-chroot /mnt mv /etc/sudoers /etc/sudoers.bac
-
-# ADD USER TO SUDOERS GROUP
-arch-chroot /mnt sed "80i$UNAME ALL=(ALL) ALL" /etc/sudoers.bac  > /etc/sudoers
-
-# EXIT CHROOT
-#exit
+#cd
+#bootctl install
+#cp /boot/loader/loader.conf /boot/loader/loader.conf.bac
+#echo -e "timeout 5\ndefault arch" > /boot/loader/loader.conf
+#touch /boot/loader/entries/arch.conf
+#echo -e "title ArchLinux\nlinux /vmlinuz-linux\ninitrd /initramfs-linux.img\n"options rw cryptdevice=UUID=$UUID":crypt" root=/dev/mapper/crypt > /boot/loader/entries/arch.conf
+#
+## MKINITCPIO
+##sed -i 's/oldstring/newstring/g' /etc/mkinitcpio.conf
+#cp /etc/mkinitcpio.conf /etc/mkinitcpio.conf.bac
+#sed -i "s/HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)/HOOKS=(base udev block keyboard autodetect modconf resume shutdown filesystems encrypt fsck keymap)/g" /etc/mkinitcpio.conf
+#mkinitcpio -p linux
+#
+## USER
+#echo -n "Enter username: "
+#read UNAME
+#useradd -m -G audio,video,wheel $UNAME
+#echo "Changing user password."
+#passwd $UNAME
+#echo "Changing root password."
+#passwd
+#
+## BACKUP SUDOERS
+#mv /etc/sudoers /etc/sudoers.bac
+#
+## ADD USER TO SUDOERS GROUP
+#sed "80i$UNAME ALL=(ALL) ALL" /etc/sudoers.bac  > /etc/sudoers
+#
+## EXIT CHROOT
+##exit
 
 # UNSET VARIABLES
 unset DISK
---
+++ chrootauto.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then
+    echo "Run as root."
+    exit 1
+fi
+
+if [ $# -ne 1 ]
+  then
+    echo "./archauto.sh <disk>"
+    exit 1
+fi
+
+DISK=$1
+UUID="$(blkid -s UUID -o value $DISK\1)"
+export DISK
+
+if [ -z "$UUID" ]; then
+	echo "Invalid disk"
+	exit 1
+fi
+
+# GET ENCRYPTED DISK UUID
+UUID="$(blkid -s UUID -o value $DISK\2)"
+export UUID
+
+# SETUP BOOTLOADER
+cd
+bootctl install
+cp /boot/loader/loader.conf /boot/loader/loader.conf.bac
+echo -e "timeout 5\ndefault arch" > /boot/loader/loader.conf
+touch /boot/loader/entries/arch.conf
+echo -e "title ArchLinux\nlinux /vmlinuz-linux\ninitrd /initramfs-linux.img\n"options rw cryptdevice=UUID=$UUID":crypt" root=/dev/mapper/crypt > /boot/loader/entries/arch.conf
+
+# MKINITCPIO
+#sed -i 's/oldstring/newstring/g' /etc/mkinitcpio.conf
+cp /etc/mkinitcpio.conf /etc/mkinitcpio.conf.bac
+sed -i "s/HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)/HOOKS=(base udev block keyboard autodetect modconf resume shutdown filesystems encrypt fsck keymap)/g" /etc/mkinitcpio.conf
+mkinitcpio -p linux
+
+# USER
+echo -n "Enter username: "
+read UNAME
+useradd -m -G audio,video,wheel $UNAME
+echo "Changing user password."
+passwd $UNAME
+echo "Changing root password."
+passwd
+
+# BACKUP SUDOERS
+mv /etc/sudoers /etc/sudoers.bac
+
+# ADD USER TO SUDOERS GROUP
+sed "80i$UNAME ALL=(ALL) ALL" /etc/sudoers.bac  > /etc/sudoers
+
+# EXIT CHROOT
+exit
