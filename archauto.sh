73bc86fd5c5c52edf53dd3a2bfa551cbcbcc9321
Pete Sassafraz <petesassafraz@protonmail.ch>
2020-06-03 04:35:31 +0000

    Change msg to echo

--- archauto.sh
+++ archauto.sh
@@ -30,27 +30,23 @@ fi
 # START TIMER
 SECONDS=0
 
-msg () {
-    echo $1; read
-}
-
 # UNMOUNT DISKS
-msg "Unmounting disks..."
+echo "Unmounting disks..."
 umount /mnt/boot
 umount /mnt
 
 # PARTITION DISKS
-msg "Partitioning disks..."
+echo "Partitioning disks..."
 echo "o \nY \nn \n1 \n \n_512M \nEF00 \nn \n3 \n \n+1G \n8200 \nn \n2 \n \n \n8300 \nw \nY" | gdisk $DISK
 lsblk $DISK
 
 # FORMAT DISKS
-msg "Formatting disks..."
+echo "Formatting disks..."
 mkfs.fat -F32 $DISK\1
 mkswap $DISK\3
 
 # ENCRYPT DISKS
-msg "Encrypting disks..."
+echo "Encrypting disks..."
 mkcrypt () {
     cryptsetup -y -v luksFormat $DISK\2
     if [ $? -eq  2 ]; then
@@ -67,11 +63,11 @@ mkcrypt
 opcrypt
 
 # FORMAT ENCRYPTED PARTITION
-msg "Formatting encrypted disk..."
+echo "Formatting encrypted disk..."
 mkfs.ext4 -O "^has_journal" /dev/mapper/crypt
 
 # MOUNT
-msg "Mounting disks..."
+echo "Mounting disks..."
 mount /dev/mapper/crypt /mnt
 if [ -d /mnt/boot ]
     then
@@ -83,11 +79,11 @@ fi
 swapon $DISK\3
 
 # UPDATE REPOS
-msg "Updating repos..."
+echo "Updating repos..."
 pacman -Syy
 
 # INSTALL
-msg "Installing..."
+echo "Installing..."
 time pacstrap /mnt base base-devel linux linux-firmware mkinitcpio vi vim pacman dhcpcd git net-tools terminator
 
 # GET ENCRYPTED DISK UUID
@@ -95,20 +91,20 @@ UUID="$(blkid -s UUID -o value $DISK\2)"
 export UUID
 
 # CONFIGURE FSTAB
-msg "Configuring fstab..."
+echo "Configuring fstab..."
 genfstab -U /mnt
 genfstab -U /mnt > /mnt/etc/fstab
 
 # ADD SCRIPT TO CHROOT
-msg "Adding script to chroot..."
+echo "Adding script to chroot..."
 cp ./chrootauto.sh /mnt/usr/local/bin/chrootauto
 
 # RUN SCRIPT
-msg "Running script in chroot..."
+echo "Running script in chroot..."
 arch-chroot /mnt chrootauto $DISK
 
 # UNSET VARIABLES
-msg "Unsetting variables..."
+echo "Unsetting variables..."
 unset DISK
 unset UUID
 unset UNAME
