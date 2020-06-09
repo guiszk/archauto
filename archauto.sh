732a98d897863520c1e42e6b05db39c3e1a78e77
Pete Sassafraz <petesassafraz@protonmail.ch>
2020-06-09 22:13:43 +0000

    Remove firefox

--- archauto.sh
+++ archauto.sh
@@ -88,7 +88,7 @@ pacman -Syy
 
 # INSTALL
 echo "Installing..."
-time pacstrap /mnt base base-devel linux linux-firmware mkinitcpio vi vim pacman dhcpcd git zsh net-tools netctl wpa_supplicant dialog terminator firefox
+time pacstrap /mnt base base-devel linux linux-firmware mkinitcpio vi vim pacman dhcpcd git zsh net-tools netctl wpa_supplicant dialog terminator
 
 # GET ENCRYPTED DISK UUID
 UUID="$(blkid -s UUID -o value $DISK\2)"
